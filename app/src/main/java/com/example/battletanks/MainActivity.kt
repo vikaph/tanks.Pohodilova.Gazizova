7dbd26b8d1fa3ff3b8ba7bfca64c14256bf0d521
357-22 <vika.p.2006@bk.ru>
2025-01-18 19:08:16 +0300

    готовое решение2

--- app/src/main/java/com/example/battletanks/MainActivity.kt
+++ app/src/main/java/com/example/battletanks/MainActivity.kt
@@ -4,20 +4,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.KeyEvent.*
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.FrameLayout
 import com.example.battletanks.Direction.UP
 import com.example.battletanks.Direction.DOWN
 import com.example.battletanks.Direction.LEFT
 import com.example.battletanks.Direction.RIGHT
 import com.example.battletanks.databinding.ActivityMainBinding
-
+lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private val gridDrawer by lazy {
+        GridDrawer(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding =  ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Menu"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.settings, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_settings -> {
+                gridDrawer.drawGrid()
+                return true
+
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
