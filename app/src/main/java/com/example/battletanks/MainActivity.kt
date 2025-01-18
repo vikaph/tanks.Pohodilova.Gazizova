cf302b593880302cc0026b86fdf78f461386ef04
357-22 <vika.p.2006@bk.ru>
2025-01-18 17:19:23 +0300

    готовое решение1

---
+++ app/src/main/java/com/example/battletanks/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.battletanks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.KeyEvent.*
+import android.widget.FrameLayout
+import com.example.battletanks.Direction.UP
+import com.example.battletanks.Direction.DOWN
+import com.example.battletanks.Direction.LEFT
+import com.example.battletanks.Direction.RIGHT
+import com.example.battletanks.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =  ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+when (keyCode) {
+    KEYCODE_DPAD_UP -> move(UP)
+    KEYCODE_DPAD_DOWN-> move(DOWN)
+    KEYCODE_DPAD_LEFT -> move(LEFT)
+    KEYCODE_DPAD_RIGHT -> move(RIGHT)
+}
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun move(direction: Direction){
+        when(direction){
+            UP->{
+ binding.myTank.rotation=0f
+                (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -50
+            }
+            DOWN->{
+                binding.myTank.rotation=180f
+                (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += 50
+            }
+            LEFT->{
+                binding.myTank.rotation=270f
+                (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin -= 50
+            }
+            RIGHT->{
+                binding.myTank.rotation=90f
+                (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin += 50
+            }
+        }
+        binding.container.removeView(binding.myTank)
+        binding.container.addView(binding.myTank)
+    }
+}
+
