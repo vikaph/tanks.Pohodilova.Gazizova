4662842ac9953b49c28ec30470147ad557bf616c
357-22 <vika.p.2006@bk.ru>
2025-03-14 10:00:16 +0300

    14.03

--- app/src/main/java/com/example/battletanks/enums/Material.kt
+++ app/src/main/java/com/example/battletanks/enums/Material.kt
@@ -1,13 +1,19 @@
 package com.example.battletanks.enums
 
+import com.example.battletanks.R
+
 enum class Material(
     val tankConGoThrough: Boolean,
 val bulletCanGoThrough: Boolean,
-val simpleBulletCanDestroy: Boolean
+val simpleBulletCanDestroy: Boolean,
+    val canExistOnlyOne: Boolean,
+    val width: Int,
+    val height: Int,
+    val image: Int
     ) {
-    EMPTY(true, true,true),
-    BRICK(false, false, true),
-    CONCRETE(false, false, false),
-    GRASS(true, true, false),
-    EAGLE (false, false, true),
+    EMPTY(true, true,true, false,0,0,0),
+    BRICK(false, false, true,false,1,1, R.drawable.brick),
+    CONCRETE(false, false, false, false,1,1,R.drawable.concrete),
+    GRASS(true, true, false,false,1,1,R.drawable.grass),
+    EAGLE (false, false, true,true,4,3,R.drawable.eagle),
 }
